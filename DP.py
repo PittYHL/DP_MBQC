a24e779090fd7520c1acc8339d94323bab02009d
yil392 <yil392@pitt.edu>
2023-09-06 16:23:55 -0400

    1.0

--- DP.py
+++ DP.py
@@ -2,6 +2,7 @@ import copy
 import networkx as nx
 from matplotlib import pyplot as plt
 from placement import *
+from To_graph import *
 
 def DP(ori_map, qubits, rows):
     new_map = []
@@ -16,7 +17,7 @@ def DP(ori_map, qubits, rows):
                 new_map[-1].append(0)
     graph, nodes, W_len, first, last, A_loc, B_loc, C_loc = gen_index(new_map)
 
-    # table, shapes, index = place_core(graph, nodes, W_len, rows, qubits, A_loc, B_loc, C_loc, force_right, restricted, special_greedy)
+    table, shapes, index = place_core(graph, nodes, W_len, rows, qubits, A_loc, B_loc, C_loc)
     # middle_shapes = shapes[-1]
     # final_shapes = place_leaves(table, shapes, first, last, rows, special)
     # show_min(middle_shapes, final_shapes)
@@ -26,135 +27,214 @@ def DP(ori_map, qubits, rows):
     # save_shapes(shapes)
     print('g')
 
-def gen_index(map):
-    cut = []
-    s_row = []
-    for i in range(1, len(map)-1, 2):
-        rowcut = []
-        j = 0
-        while j < len(map[0]):
-            if map[i][j] != 0:
-                rowcut.append(j)
-                # if j + 1 < len(map[0]):
-                #     if map[i][j+1] == 1:
-                #         j = j + 1
-            j = j + 1
-        cut.append(rowcut)
+def place_core(graph, nodes, W_len, rows, qubits, A_loc, B_loc, C_loc):
+    # n_nodes = np.array(nodes)
+    # np.savetxt("example/iqp20_nodes.csv", n_nodes, fmt = '%s',delimiter=",")
+    order = list(nx.topological_sort(graph))
+    order, W_len, graph, nodes = update_all_wires(order, W_len, graph, nodes)#switch wire to the right
+    independet_node = find_independent(graph, order)
+    table = [[]]
+    shape = [[]]
+    valid = [[]]  # for chosen patterns
+    two_wire = []  # record the node with two wire predecessors
+    qubit_record = []
+    placed = []
+    nodes_left = copy.deepcopy(order)
+    #place each independent node until two-qubit pattern
+    inde_table = []
+    inde_shape = []
+    for i in range(len(independet_node)):
+        inde_table.append([])
+        inde_shape.append([])
+        current = independet_node.pop(0)
+        nodes_left.remove(current)
+        qubit_record = get_qubit_record(current, nodes, qubit_record)
+        inde_table[0], inde_shape[0], new_placed = place_independent(current, graph, qubit_record, rows, qubits, nodes, nodes_left, A_loc, B_loc, C_loc)
+    print('g')
 
-    for i in range(0, len(cut) - 1):
-        temp_row = cut[i] + cut[i + 1]
-        temp_row.sort()
-        s_row.append(copy.deepcopy(temp_row))
-    s_row.insert(0, cut[0])
-    s_row.append(cut[-1]) #the two qubit gate location for each row
-    first = []
-    last = []
-    qubit = 0
-    for i in range(0, len(map), 2):
-        for j in range(len(map[i])):
-            if map[i][j] != 0:
-                front = s_row[qubit][0] - j
-                break
-        for j in reversed(range(len(map[i]))):
-            if map[i][j] != 0:
-                back = j - s_row[qubit][-1]
-                break
-        qubit = qubit + 1
-        first.append(front) #length for the patterns in the front
-        last.append(back) #length for the patterns in the back
-    nodes, W_len, A_loc, B_loc, C_loc = gen_DAG(map,s_row)
-    graph = nx.DiGraph()
-    for node in nodes:
-        for i in range(len(node) - 1):
-            graph.add_edge(node[i], node[i+1])
-    #order = list(nx.topological_sort(graph))
-    #next = list(graph.successors('A.0'))
-    return graph, nodes, W_len, first, last, A_loc, B_loc, C_loc
+def place_independent(current, graph, qubit_record, rows, qubits, nodes, nodes_left, A_loc, B_loc, C_loc):
+    placed = []
+    placed.append(current)
+    gate, _ = current.split('.')
+    succ = list(graph.successors(current))
+    temp_shape = []
+    table = [[]]
+    shape = [[]]
+    if gate == 'A':
+        dep = 1
+        temp_shape.append([1])
+        temp_shape.append([1])
+        temp_shape.append([1])
+        if len(succ) == 2:
+            table[0].append({'New': current, 'P': 'NA', 'row': 3, 'S': 0, 'D': dep, 'Q': 2, 'front': [[0, 0], [2, 0]], 'successor':[succ[0], succ[1]], 'targets':[], 'starts':[[0, 0], [2, 0]], 'ends':[]})
+        elif len(succ) == 1:
+            end = detec_end(current, succ[0], nodes)
+            if end == 'u':
+                table[0].append({'New': current, 'P': 'NA', 'row': 3, 'S': 0, 'D': dep, 'Q': 2, 'front': [[2, 0]], 'successor':[succ[0]], 'targets':[], 'starts':[[0, 0], [2, 0]], 'ends':[[0, 0]]})
+            else:
+                table[0].append({'New': current, 'P': 'NA', 'row': 3, 'S': 0, 'D': dep, 'Q': 2, 'front': [[0, 0]], 'successor':[succ[0]], 'targets':[], 'starts':[[0, 0], [2, 0]], 'ends':[[2, 0]]})
+    else:
+        dep = 2
+        temp_shape.append([1,1])
+        temp_shape.append([1,1])
+        temp_shape.append([1,1])
+        if len(succ) == 2:
+            table[0].append({'New': current, 'P': 'NA', 'row': 3, 'S': 0, 'D': dep, 'Q': 2, 'front': [[0, 1], [2, 1]], 'successor':[succ[0], succ[1]], 'targets':[], 'starts':[[0, 0], [2, 0]], 'ends':[]})
+        elif len(succ) == 1:
+            end = detec_end(current, succ[0], nodes)
+            if end == 'u':
+                table[0].append(
+                    {'New': current, 'P': 'NA', 'row': 3, 'S': 0, 'D': dep, 'Q': 2, 'front': [[2, 1]],
+                     'successor': [succ[0]], 'targets': [], 'starts': [[0, 0], [2, 0]], 'ends': [[0,1]]})
+            else:
+                table[0].append(
+                    {'New': current, 'P': 'NA', 'row': 3, 'S': 0, 'D': dep, 'Q': 2, 'front': [[0, 1]],
+                     'successor': [succ[0]], 'targets': [], 'starts': [[0, 0], [2, 0]], 'ends': [[2,1]]})
+    shape[0].append(temp_shape)
+    till_end = 1
+    for node in succ:
+        preds = list(graph.predecessors(node))
+        for pred in preds:
+            if pred not in placed:
+                till_end = 0
+    while till_end:
+        next = choose_next(nodes_left, placed, graph, nodes, A_loc, B_loc, C_loc)  # chose the next
+        c_qubit = find_qubits(nodes, placed, next)
+        new_sucessors = list(graph.successors(next))
+        loc = check_loc(nodes, placed, next, graph)
 
-def gen_DAG(map, s_row):
+    return table, shape, placed
+
+def choose_next(nodes_left, placed, graph, nodes, A_loc, B_loc, C_loc, two_wire):
+    next = []
+    parent_index = [] #the parent of the chosen
+    parent_row = [] #record the row number of the qubit
+    found_wire = 0 #choose the wire
+    found_C = 0 #choose the C
+    only_one = [] #the node that only one preds have been placed
+    succ_placed = [] #the node that succs have been placed
+    for node in nodes_left: #found all the nodes that predecessors have been resolved
+        succs = list(graph.successors(node))
+        before = list(graph.predecessors(node))
+        p_index = 100000
+        wires = 0
+        gate, _ = node.split('.')
+        solved = 1
+        if before == []:
+            solved = 0
+        pred_placed = []
+        for pred in before:
+            gate1, _ = pred.split('.')
+            if pred in placed:
+                index = placed.index(pred)
+                pred_placed.append(pred)
+            elif pred not in placed and gate1 != 'W': #if one of the predecessor is wire
+                solved = 0
+            elif gate1 == 'W':
+                wires = wires + 1
+            if pred in placed and p_index > index:
+                p_index = index
+        if len(before) == 2 and pred_placed != [] and (before[0] in two_wire or before[1] in two_wire): #for two wire
+            solved = 1
+        if len(pred_placed) == 1 and len(before) == 2:
+            only_one.append(node)
+            solved = 1
+        elif len(pred_placed) == 0 and gate != 'W':#if none of the predecessors have been placed, look for succesor
+            for succ in succs:
+                if succ in placed:
+                    solved = 1
+                    succ_placed.append(node)
+        if wires == len(before) and before != []: #both predecessors are wires and one of the sucessors is placed
+            solved = 0
+            if node not in two_wire:
+                two_wire.append(node)
+            elif len(succs) == 1:
+                if wires == len(before) and succs[0] in placed:
+                    found_wire = 1
+                    next_node = node
+                    break
+            elif len(succs) == 2:
+                if wires == len(before) and (succs[0] in placed or succs[1] in placed):
+                    found_wire = 1
+                    next_node = node
+                    break
+        elif solved:
+            gate1, num = node.split('.')
+            pred = list(graph.predecessors(node))
+            if pred != []:
+                gate2, _ = pred[0].split('.')
+            if succs != []:
+                gate3, _ = succs[0].split('.')
+            if gate1 == 'B' or gate1 == 'B1':
+                loc = B_loc[int(num)]
+            elif gate1 == 'A':
+                loc = A_loc[int(num)]
+            elif gate1 == 'C':
+                loc = C_loc[int(num)]
+            if gate1 == 'W':
+                succ = list(graph.successors(node)) #choose the wire whose succesor has been placed
+                if succ[0] in placed:
+                    found_wire = 1
+                    next_node = node
+                    # break
+            elif gate1 == 'C' and ((pred[0] in placed and gate2 == 'C') or (succs[0] in placed and gate3 == 'C')): #choose C if the previous is also C
+                found_C = 1
+                next_node = node
+                break
+            else:
+                next.append(node)
+                parent_index.append(loc)
+                for i in range(len(nodes)): #record the row
+                    if node in nodes[i]:
+                        parent_row.append(i)
+                        break
+    if found_wire != 1 and found_C != 1:
+        if succ_placed != []:
+            next_node = find_higher_node(parent_index, succ_placed, next, parent_row)
+        elif parent_index != []:
+            next_node = available_node(parent_index, next, parent_row)
+        elif only_one != []: # cannot find node with both predecessors resolved or wires or
+            next_node = only_one[-1]
+    return next_node
+
+def find_higher_node(parent_index, succ_placed, next, parent_row):
+    if len(succ_placed) == 1:
+        return succ_placed[0]
+    else:
+        indexes = []
+        for node in succ_placed:
+            indexes.append(next.index(node))
+        new_parent_index = []
+        new_parent_row = []
+        for i in indexes:
+            new_parent_index.append(parent_index[i])
+            new_parent_row.append(parent_row)
+        max_index = max(new_parent_index)
+        temp_parent = []
+        max_indexes = []
+        for i in range(len(new_parent_index)):
+            if new_parent_index[i] == max_index:
+                max_indexes.append(i)
+        for i in max_indexes:
+            temp_parent.append(new_parent_row[i])
+        return succ_placed[max_indexes[temp_parent.index(min(temp_parent))]]
+
+def available_node(parent_index, next, parent_row):
+    two_qubit = []
+    two_qubit_index = []
+    two_qubit_row= []
+    for i in range(len(next)):
+        gate1, _ = next[i].split('.')
+        if gate1 == 'A' or gate1 == 'B' or gate1 == 'B1':
+            two_qubit.append(next[i])
+            two_qubit_index.append(parent_index[i])
+            two_qubit_row.append(parent_row[i])
+    min_index = min(parent_index)
+    temp_parent = []
     indexes = []
-    for index in s_row:
-        indexes = indexes + index
-    indexes = [*set(indexes)]
-    indexes.sort()
-    nodes = [] #graph nodes
-    node_loc = []
-    for i in range(len(s_row)):
-        nodes.append([])
-        node_loc.append([])
-    A = 0
-    B = 0
-    C = 0
-    W = 0
-    A_loc = []
-    B_loc = []
-    C_loc = [] #none wire single row length
-    W_loc = []
-    W_len = [] #wire single row length
-    index = 0
-    while index < len(indexes):
-        for i in range(1, len(map)-1, 2):
-            next_0 = check_next_0(map, i, indexes[index])
-            if map[i][indexes[index]] != 0:
-                if map[i][indexes[index] + 1] != 0 and next_0 % 2 == 0 and ((i == 1 and map[i+2][indexes[index]] == 0) or
-                (i == len(map)-2 and map[i-2][indexes[index]] == 0)
-                or (1 < i < len(map)-2 and map[i+2][indexes[index]] == 0 and map[i-2][indexes[index]] == 0)):
-                    node = 'B.' + str(B)
-                    loc = int((i-1)/2)
-                    nodes[loc].append(node)
-                    nodes[loc + 1].append(node)
-                    node_loc[loc].append(indexes[index] + 1)
-                    node_loc[loc + 1].append(indexes[index] + 1)
-                    B_loc.append(indexes[index])
-                    B = B + 1
-                elif map[i][indexes[index] - 1] == 0 and map[i][indexes[index] + 1] == 0:
-                    node = 'A.' + str(A)
-                    loc = int((i - 1) / 2)
-                    nodes[loc].append(node)
-                    nodes[loc + 1].append(node)
-                    node_loc[loc].append(indexes[index])
-                    node_loc[loc + 1].append(indexes[index])
-                    A_loc.append(indexes[index])
-                    A =  A + 1
-        for i in range(1, len(map)-1, 2):
-            next_0 = check_next_0(map, i, indexes[index])
-            if map[i][indexes[index]] != 0:
-                if map[i][indexes[index] + 1] != 0 and next_0 % 2 == 0 and ((i == 1 and map[i+2][indexes[index]] != 0) or
-                (i == len(map)-2 and map[i-2][indexes[index]] != 0) or (1 < i < len(map)-2 and (map[i+2][indexes[index]] != 0 or map[i-2][indexes[index]] != 0))):
-                    node = 'B1.' + str(B)
-                    loc = int((i - 1) / 2)
-                    nodes[loc].append(node)
-                    nodes[loc + 1].append(node)
-                    node_loc[loc].append(indexes[index] + 1)
-                    node_loc[loc + 1].append(indexes[index] + 1)
-                    B_loc.append(indexes[index])
-                    B = B + 1
-        index = index + 1
-    for i in range(len(s_row)):
-        add = 1
-        for j in range(len(s_row[i]) - 1):
-            if s_row[i][j + 1] - s_row[i][j] > 1:
-                if map[i * 2][s_row[i][j] + 1] == 2 and s_row[i][j + 1] - s_row[i][j] > 3:
-                    node = 'W.' + str(W)
-                    W = W + 1
-                    loc = node_loc[i].index(s_row[i][j])
-                    nodes[i].insert(loc + add, node)
-                    add = add + 1
-                    W_len.append(s_row[i][j + 1] - s_row[i][j] - 1)
-                #elif map[i * 2][s_row[i][j] + 1] != 2:
-                else:
-                    for k in range(s_row[i][j + 1] - s_row[i][j] - 1):
-                        C_loc.append(k + s_row[i][j] + 1)
-                        node = 'C.' + str(C)
-                        C = C + 1
-                        loc = node_loc[i].index(s_row[i][j])
-                        nodes[i].insert(loc + add, node)
-                        add = add + 1
-                    #C_len.append(s_row[i][j + 1] -  s_row[i][j] - 1)
-    return nodes, W_len, A_loc, B_loc, C_loc
-
-def check_next_0(map, i, start):
-    index = copy.deepcopy(start)
-    while(map[i][index] != 0):
-        index = index + 1
-    return index - start
+    for i in range(len(parent_index)):
+        if parent_index[i] == min_index:
+            indexes.append(i)
+    for i in indexes:
+        temp_parent.append(parent_row[i])
+    return next[indexes[temp_parent.index(min(temp_parent))]]
---
+++ To_graph.py
@@ -0,0 +1,301 @@
+import copy
+import networkx as nx
+from matplotlib import pyplot as plt
+
+def gen_index(map):
+    cut = []
+    s_row = []
+    for i in range(1, len(map)-1, 2):
+        rowcut = []
+        j = 0
+        while j < len(map[0]):
+            if map[i][j] != 0:
+                rowcut.append(j)
+                # if j + 1 < len(map[0]):
+                #     if map[i][j+1] == 1:
+                #         j = j + 1
+            j = j + 1
+        cut.append(rowcut)
+
+    for i in range(0, len(cut) - 1):
+        temp_row = cut[i] + cut[i + 1]
+        temp_row.sort()
+        s_row.append(copy.deepcopy(temp_row))
+    s_row.insert(0, cut[0])
+    s_row.append(cut[-1]) #the two qubit gate location for each row
+    first = []
+    last = []
+    qubit = 0
+    for i in range(0, len(map), 2):
+        for j in range(len(map[i])):
+            if map[i][j] != 0:
+                front = s_row[qubit][0] - j
+                break
+        for j in reversed(range(len(map[i]))):
+            if map[i][j] != 0:
+                back = j - s_row[qubit][-1]
+                break
+        qubit = qubit + 1
+        first.append(front) #length for the patterns in the front
+        last.append(back) #length for the patterns in the back
+    nodes, W_len, A_loc, B_loc, C_loc = gen_DAG(map,s_row)
+    graph = nx.DiGraph()
+    for node in nodes:
+        for i in range(len(node) - 1):
+            graph.add_edge(node[i], node[i+1])
+    #order = list(nx.topological_sort(graph))
+    #next = list(graph.successors('A.0'))
+    return graph, nodes, W_len, first, last, A_loc, B_loc, C_loc
+
+def gen_DAG(map, s_row):
+    indexes = []
+    for index in s_row:
+        indexes = indexes + index
+    indexes = [*set(indexes)]
+    indexes.sort()
+    nodes = [] #graph nodes
+    node_loc = []
+    for i in range(len(s_row)):
+        nodes.append([])
+        node_loc.append([])
+    A = 0
+    B = 0
+    C = 0
+    W = 0
+    A_loc = []
+    B_loc = []
+    C_loc = [] #none wire single row length
+    W_loc = []
+    W_len = [] #wire single row length
+    index = 0
+    while index < len(indexes):
+        for i in range(1, len(map)-1, 2):
+            next_0 = check_next_0(map, i, indexes[index])
+            if map[i][indexes[index]] != 0:
+                if map[i][indexes[index] + 1] != 0 and next_0 % 2 == 0 and ((i == 1 and map[i+2][indexes[index]] == 0) or
+                (i == len(map)-2 and map[i-2][indexes[index]] == 0)
+                or (1 < i < len(map)-2 and map[i+2][indexes[index]] == 0 and map[i-2][indexes[index]] == 0)):
+                    node = 'B.' + str(B)
+                    loc = int((i-1)/2)
+                    nodes[loc].append(node)
+                    nodes[loc + 1].append(node)
+                    node_loc[loc].append(indexes[index] + 1)
+                    node_loc[loc + 1].append(indexes[index] + 1)
+                    B_loc.append(indexes[index])
+                    B = B + 1
+                elif map[i][indexes[index] - 1] == 0 and map[i][indexes[index] + 1] == 0:
+                    node = 'A.' + str(A)
+                    loc = int((i - 1) / 2)
+                    nodes[loc].append(node)
+                    nodes[loc + 1].append(node)
+                    node_loc[loc].append(indexes[index])
+                    node_loc[loc + 1].append(indexes[index])
+                    A_loc.append(indexes[index])
+                    A =  A + 1
+        for i in range(1, len(map)-1, 2):
+            next_0 = check_next_0(map, i, indexes[index])
+            if map[i][indexes[index]] != 0:
+                if map[i][indexes[index] + 1] != 0 and next_0 % 2 == 0 and ((i == 1 and map[i+2][indexes[index]] != 0) or
+                (i == len(map)-2 and map[i-2][indexes[index]] != 0) or (1 < i < len(map)-2 and (map[i+2][indexes[index]] != 0 or map[i-2][indexes[index]] != 0))):
+                    node = 'B1.' + str(B)
+                    loc = int((i - 1) / 2)
+                    nodes[loc].append(node)
+                    nodes[loc + 1].append(node)
+                    node_loc[loc].append(indexes[index] + 1)
+                    node_loc[loc + 1].append(indexes[index] + 1)
+                    B_loc.append(indexes[index])
+                    B = B + 1
+        index = index + 1
+    for i in range(len(s_row)):
+        add = 1
+        for j in range(len(s_row[i]) - 1):
+            if s_row[i][j + 1] - s_row[i][j] > 1:
+                if map[i * 2][s_row[i][j] + 1] == 2 and s_row[i][j + 1] - s_row[i][j] > 3:
+                    node = 'W.' + str(W)
+                    W = W + 1
+                    loc = node_loc[i].index(s_row[i][j])
+                    nodes[i].insert(loc + add, node)
+                    add = add + 1
+                    W_len.append(s_row[i][j + 1] - s_row[i][j] - 1)
+                #elif map[i * 2][s_row[i][j] + 1] != 2:
+                else:
+                    for k in range(s_row[i][j + 1] - s_row[i][j] - 1):
+                        C_loc.append(k + s_row[i][j] + 1)
+                        node = 'C.' + str(C)
+                        C = C + 1
+                        loc = node_loc[i].index(s_row[i][j])
+                        nodes[i].insert(loc + add, node)
+                        add = add + 1
+                    #C_len.append(s_row[i][j + 1] -  s_row[i][j] - 1)
+    return nodes, W_len, A_loc, B_loc, C_loc
+
+def check_next_0(map, i, start):
+    index = copy.deepcopy(start)
+    while(map[i][index] != 0):
+        index = index + 1
+    return index - start
+
+def update_all_wires(order, W_len, graph, nodes):
+    to_be_removed= []
+    for node in order:
+        gate, _ = node.split('.')
+        if gate != 'A' and gate != 'B':
+            continue
+        else:
+            all_wire = 1
+            pre = list(graph.predecessors(node))
+            suc = list(graph.successors(node))
+            if pre == [] or suc == []:
+                continue
+            for p in pre:
+                gate, _ = p.split('.')
+                if gate != 'W':
+                    all_wire = 0
+                    break
+            for s in suc:
+                gate, _ = s.split('.')
+                if gate != 'W':
+                    all_wire = 0
+                    break
+            if all_wire == 1:
+                min_len = 100000
+                for p in pre:
+                    gate, index = p.split('.')
+                    if W_len[int(index)] < min_len:
+                        min_len = W_len[int(index)]
+                for p in pre:
+                    gate, index = p.split('.')
+                    W_len[int(index)] = W_len[int(index)] - min_len
+                    if W_len[int(index)] == 0:
+                        to_be_removed.append('W.' + index)
+                for s in suc:
+                    gate, index = s.split('.')
+                    W_len[int(index)] = W_len[int(index)] + min_len
+    if to_be_removed != []:
+        for node in to_be_removed:
+            for j in range(len(nodes)):
+                if node in nodes[j]:
+                    nodes[j].remove(node)
+        new_graph = nx.DiGraph()
+        for node in nodes:
+            for i in range(len(node) - 1):
+                new_graph.add_edge(node[i], node[i + 1])
+        order = list(nx.topological_sort(new_graph))
+        return order, W_len, new_graph, nodes
+    return order, W_len, graph, nodes
+
+def find_independent(graph, order):
+    independent_node = []
+    for node in order:
+        pre = list(graph.predecessors(node))
+        if pre == []:
+            independent_node.append(node)
+        else:
+            break
+    return independent_node
+
+def get_qubit_record(current, nodes, qubit_record):
+    for i in range(len(nodes)):
+        if current in nodes[i]:
+            if i + 1 not in qubit_record:
+                qubit_record.append(i + 1)
+    return qubit_record
+
+def detec_end(next, succ, nodes):
+    first_qubit = 0
+    second_qubit = 0
+    gate, _ = succ.split('.')
+    for i in range(len(nodes)):
+        if next in nodes[i]:
+            first_qubit = first_qubit + i
+        if succ in nodes[i]:
+            second_qubit = second_qubit + i
+    if gate == 'A' or gate == 'B':
+        if second_qubit - first_qubit < 0:
+            end = 'd'
+        elif second_qubit - first_qubit > 0:
+            end = 'u'
+        else:
+            end = 0
+    else:
+        if first_qubit == second_qubit * 2 + 1:
+            end = 'd'
+        else:
+            end = 'u'
+    return end
+
+def check_loc(nodes, placed, next, graph, two_wire):
+    newnext = 0
+    preds = list(graph.predecessors(next))
+    previous = 0
+    no_pred = 0
+    if next in two_wire:    #put back
+        previous = next
+        succs = list(graph.successors(next))
+        for succ in succs:
+            if succ in placed:
+                newnext = succ
+                break
+    else:
+        newnext = next
+        for pred in preds:
+            if pred in placed:
+                previous = pred
+                break
+    if previous == 0: #no preds have been placed
+        no_pred = 1 #no previous indication
+        previous = next
+        succs = list(graph.successors(next))
+        for succ in succs:
+            if succ in placed:
+                newnext = succ
+                break
+    p_gate, _ = previous.split('.')
+    c_gate, _ = newnext.split('.')
+    if p_gate == 'A' or p_gate == 'B' or p_gate == 'B1':
+        qubit1 = 0
+        for i in range(len(nodes)):
+            if previous in nodes[i]:
+                qubit1 = i
+                break
+        if newnext in nodes[qubit1]:
+            loc = 'u'
+        else:
+            loc = 'd'
+    elif c_gate == 'A' or p_gate == 'B' or p_gate == 'B1':
+        qubit1 = 0
+        for i in range(len(nodes)):
+            if newnext in nodes[i]:
+                qubit1 = i
+                break
+        if previous in nodes[qubit1]:
+            loc = 'd'
+        else:
+            loc = 'u'
+    elif p_gate == 'C' and (c_gate == 'B' or c_gate == 'A' or p_gate == 'B1'):
+        qubit1 = 0
+        for i in range(len(nodes)):
+            if previous in nodes[i]:
+                qubit1 = i
+                break
+        if qubit1 == len(nodes) - 1 or newnext in nodes[qubit1 - 1]:
+            loc = 'u'
+        else:
+            loc = 'd'
+    else:
+        loc = 'r'
+    if (next in two_wire or no_pred) and loc == 'd':
+        loc = 'u'
+    elif (next in two_wire or no_pred) and loc == 'u':
+        loc = 'd'
+    return loc
+
+def find_qubits(nodes, placed, next):
+    qubit = []
+    new_placed = placed + [next]
+    for i in range(len(nodes)):
+        for node in new_placed:
+            if node in nodes[i]:
+                qubit.append(i)
+                break
+    return len(qubit)
