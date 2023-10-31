93cef888175c6c23865bf9599ab59798e2506cb9
PittYHL <yil392@pitt.edu>
2023-10-30 22:13:43 -0400

    1.3

--- reorder.py
+++ reorder.py
@@ -32,7 +32,7 @@ for i in range(qubits*2-1):
     map.append([])
 for i in range(qubits):
     tracker.append(i)
-with open('Benchmarks/qft100.txt') as f:
+with open('Benchmarks/bv100b.txt') as f:
     lines = f.readlines()
 circuit= lines.copy()
 layer = []
